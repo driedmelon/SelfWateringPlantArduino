1277316002af07f855af8cb7ef2f6a0b0bf853fb
driedmelon <chun.hongwei@u.nus.edu>
2018-10-23 19:20:02 +0800

    Added LCD Display for soil moisture level

--- sensor_demo.ino
+++ sensor_demo.ino
@@ -1,69 +1,48 @@
 #include <Wire.h>  
 #include <LiquidCrystal_I2C.h> // Using version 1.2.1
- 
-// The LCD constructor - address shown is 0x27 - may or may not be correct for yours
-// Also based on YWRobot LCM1602 IIC V1
+
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  
 
-int sensorValue = 0;
-int countOn;
-int countOff;
-int flag;
-int timer;
-int timer2;
+int sensorVal = 0;
+int sensorPin = A0;
+int motorTrig = 13;
+volatile int pumpState = 0;
 
-void setup()
-{
+void setup() {
   lcd.begin(16,2); // sixteen characters across - 2 lines
   lcd.backlight();
   // first character - 1st line
   lcd.setCursor(0,0);
-  lcd.print("Machine Status:");
-  // 8th character - 2nd line 
+  lcd.print("Water Level:");
   lcd.setCursor(1,1);
   lcd.print("--LOADING--");
-
+  
+  pinMode(motorTrig, OUTPUT);
   Serial.begin(9600);
 }
- 
- 
-void loop()
-{
-  sensorValue = analogRead(A0);
-  //Serial.println(sensorValue);
-  if (sensorValue < 200){
-    countOn++;
-    flag = 1;
-    if (countOn > 50){
-      lcd.setCursor(1,1);
-      lcd.print("-- IN USE --");
-    }
-    timer = millis();
-  }
-  if (sensorValue > 200){
-    countOff++;
-    flag = 2;
-    if (countOff > 50){
-      lcd.setCursor(1,1);
-      lcd.print("- AVAILABLE -");
-    }
-    timer2 = millis();
-  }
-  int value = timer2 - timer;
-  if (value < 0)
-    value = value*-1;
-  Serial.print(flag);
-  Serial.print("   ");
-  Serial.println(value);
-  if (value < 600){
-    lcd.setCursor(1,1);
-    lcd.print("- FINISHING -");
-    countOn = 0;
-    countOff = 0;
-  }
 
+void loop() {
+  sensorVal = analogRead(sensorPin);
+  Serial.print(pumpState);
+  Serial.print(" ");
+  Serial.println(sensorVal);
+  lcd.setCursor(0,1);
+  lcd.print(sensorVal);
+  delay(50);
+  lcd.setCursor(3,1);
+  lcd.print("------>");
+  delay(200);
+  //dry = 1023 wet = 0
+  if (sensorVal > 700)  { // EDIT SENSORVAL BASED ON YOUR PLANT. 
+    digitalWrite(motorTrig,HIGH);
+    pumpState = 1;
+    lcd.print("ON");
+    delay(200);
+  }
+  else if (sensorVal <= 600)  {
+    digitalWrite(motorTrig,LOW);
+    pumpState = 0;
+    lcd.print("OFF");
+    delay(200);
+  }
 }
-
-
-
-
